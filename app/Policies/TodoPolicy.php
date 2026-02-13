461852315abead5f1008f4d01b79b75c60c30ce2
gamatech89 <bojanmark89@gmail.com>
2026-02-13 14:13:44 +0100

    revert: developers should NOT be able to delete todos

--- app/Policies/TodoPolicy.php
+++ app/Policies/TodoPolicy.php
@@ -87,11 +87,11 @@ class TodoPolicy
 
     /**
      * Determine whether the user can delete the todo.
-     * Managers and developers (of their projects) can delete.
+     * Only admins and managers (of their projects) can delete.
      */
     public function delete(User $user, Todo $todo): bool
     {
-        if ($user->role === 'viewer') {
+        if ($user->role === 'viewer' || $user->role === 'developer') {
             return false;
         }
 
@@ -99,11 +99,6 @@ class TodoPolicy
 
         if ($user->role === 'manager') {
             return $project->managers->contains('id', $user->id);
-        }
-        
-        if ($user->role === 'developer') {
-            return $project->developer_id === $user->id
-                || $project->developers()->where('user_id', $user->id)->exists();
         }
         
         return false;
